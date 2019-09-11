1862da330593d6a1500a87184302ac64e39dbd6f
Dominique Maez <dominiquemaez@gmail.com>
2019-09-10 18:35:10 -0700

    made the javascript for the nav bar and linked nav to id selectors

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $(window).on('scroll', function(){
+        if($(window).scrollTop()>= 800){ // Carousel Size
+            $('.navbar').css({ 
+                'background-color': '#000000',
+                'padding-bottom': '5px'
+            });
+        } else {
+            $('.navbar').css({
+                'background-color': 'transparent',
+            });
+        }
+    });
+});
+
