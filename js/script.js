c9e3e13153c1c38d516f299144287784dc4f8a66
Ayanna Shaw <ayanna.shaw21@gmail.com>
2019-09-24 13:18:31 -0700

    commented out css styles that caused format issues

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $(window).on('scroll', function () {
-        if ($(window).scrollTop() >= 800) { // Carousel Size
+        if ($(window).scrollTop() >= 750) { // Carousel Size
             $('.navbar').css({
                 'background-color': '#000000',
                 'padding-bottom': '5px'
